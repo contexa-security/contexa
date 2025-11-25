567975d770724b8f307c604bf661b12408078eb9
onjsdnjs <onjsdnjs@gmail.com>
2025-11-25 11:21:06 +0900

    initial commit

--- contexa-core/src/main/java/io/contexa/contexacore/properties/SecurityPipelineProperties.java
+++ contexa-core/src/main/java/io/contexa/contexacore/properties/SecurityPipelineProperties.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
+
 /**
  * Security Pipeline 설정
  */
