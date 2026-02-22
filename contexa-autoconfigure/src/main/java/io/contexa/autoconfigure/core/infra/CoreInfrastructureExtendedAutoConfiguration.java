2eec741f65beb5e40244f35cbb1bf1b8da731db5
onjsdnjs <onjsdnjs@gmail.com>
2026-02-22 19:26:01 +0900

    initial commit

--- contexa-autoconfigure/src/main/java/io/contexa/autoconfigure/core/infra/CoreInfrastructureExtendedAutoConfiguration.java
+++ contexa-autoconfigure/src/main/java/io/contexa/autoconfigure/core/infra/CoreInfrastructureExtendedAutoConfiguration.java
@@ -1,5 +1,6 @@
 package io.contexa.autoconfigure.core.infra;
 
+import io.contexa.autoconfigure.properties.ContexaEnterpriseProperties;
 import io.contexa.autoconfigure.properties.ContexaProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -7,5 +8,5 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @AutoConfiguration
 @ConditionalOnProperty(prefix = "contexa.infrastructure", name = "enabled", havingValue = "true", matchIfMissing = true)
-@EnableConfigurationProperties(ContexaProperties.class)
+@EnableConfigurationProperties({ContexaProperties.class, ContexaEnterpriseProperties.class})
 public class CoreInfrastructureExtendedAutoConfiguration {}
