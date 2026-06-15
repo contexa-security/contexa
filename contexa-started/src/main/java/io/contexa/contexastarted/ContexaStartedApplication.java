1dac89498915a84557a507fdd93739c331724a16
onjsdnjs <onjsdnjs@gmail.com>
2026-06-15 16:32:33 +0900

    code refactoring

---
+++ contexa-started/src/main/java/io/contexa/contexastarted/ContexaStartedApplication.java
@@ -0,0 +1,13 @@
+package io.contexa.contexastarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContexaStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContexaStartedApplication.class, args);
+    }
+
+}
---
+++ contexa-started/src/test/java/io/contexa/contexastarted/ContexaStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package io.contexa.contexastarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContexaStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
